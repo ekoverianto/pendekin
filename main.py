80b1952870b3ce475b2a25ed4d317930258a464f
ekoverianto <ekoverianto@mail.ugm.ac.id>
2022-06-13 00:01:43 +0700

    change to one function

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import pyshorteners
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        url = request.form['url']
+        result = pyshorteners.Shortener().tinyurl.short(url)
+        return render_template('index.html', result = result)
 
-@app.route('/result', methods=['POST'])
-def result():
-    url = request.form['url']
-    result = pyshorteners.Shortener().tinyurl.short(url)
-    return render_template('result.html', result = result)
+# @app.route('/result', methods=['POST'])
+# def result():
+#     url = request.form['url']
+#     result = pyshorteners.Shortener().tinyurl.short(url)
+#     return render_template('result.html', result = result)
 
 if __name__ == '__main__':
     app.run(debug=True)
